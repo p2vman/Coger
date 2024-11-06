a2a16442b679c30d5e7913f47d93bf9208ef42f2
p2vman <andreywontomsky@gmail.com>
2024-11-06 14:12:48 +0200

    jk

--- cogerm/__init__.py
+++ cogerm/__init__.py
@@ -1,5 +1,7 @@
 import os
 import json
+import zipfile
+from io import TextIOWrapper
 
 class DynamicObject:
     def __init__(self, attributes):
@@ -35,14 +37,20 @@ class CogLoader:
         func.emit = True
         return func
 
-    def emit(self, method : str, *args) -> None:
+    def emit(self, method: str, *args) -> None:
         for obj in self.cog_list:
             if hasattr(obj, method):
                 func = getattr(obj, method)
                 if hasattr(func, 'emit'):
                     func(*args)
 
-    def load(self, dir_: str, meta) -> None:
+    def load(self, source: str, meta) -> None:
+        if source.endswith(".zip"):
+            self.load_from_zip(source, meta)
+        else:
+            self.load_from_directory(source, meta)
+
+    def load_from_directory(self, dir_: str, meta) -> None:
         base_ctx = CogContext(self.bot, {}, meta)
 
         for filename in os.listdir(dir_):
@@ -53,35 +61,51 @@ class CogLoader:
             elif filename.endswith("cog.json"):
                 self.load_cog_from_json(file_path, base_ctx)
 
-    def load_cog_from_python(self, file_path, base_ctx):
-        with open(file_path, 'r', encoding='utf-8') as file:
-            namespace = {}
-            exec(file.read(), namespace)
-            class_dict = {name: obj for name, obj in namespace.items() if isinstance(obj, type)}
-            func_dict = {name: obj for name, obj in namespace.items() if callable(obj)}
+    def load_from_zip(self, zip_path: str, meta) -> None:
+        base_ctx = CogContext(self.bot, {}, meta)
 
-            json_filepath = file_path[:-5] + ".json"
-            if os.path.exists(json_filepath):
-                with open(json_filepath, 'r', encoding='utf-8') as f:
-                    data = json.load(f)
-                    self.process_entries(data, base_ctx, class_dict, func_dict)
-            elif file_path[:-5] in class_dict:
-                class_dict[file_path[:-5]](base_ctx).init(base_ctx)
+        with zipfile.ZipFile(zip_path, 'r') as zip_file:
+            for filename in zip_file.namelist():
+                if filename.endswith("_c.py"):
+                    with zip_file.open(filename) as file:
+                        self.load_cog_from_python(TextIOWrapper(file, encoding='utf-8'), base_ctx, filename)
+                elif filename.endswith("cog.json"):
+                    with zip_file.open(filename) as file:
+                        self.load_cog_from_json(TextIOWrapper(file, encoding='utf-8'), base_ctx)
 
-    def load_cog_from_json(self, json_path, base_ctx):
-        with open(json_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            for cog_name, cog_info in data.items():
-                cog_file_path = os.path.join(os.path.dirname(json_path), cog_name)
-                if os.path.exists(cog_file_path):
-                    print(f"Load {cog_info['name']}")
-                    with open(cog_file_path, 'r', encoding='utf-8') as file:
-                        namespace = {}
-                        exec(file.read(), namespace)
-                        class_dict = {name: obj for name, obj in namespace.items() if isinstance(obj, type)}
-                        func_dict = {name: obj for name, obj in namespace.items() if callable(obj)}
+    def load_cog_from_python(self, file, base_ctx, file_name=None):
+        if isinstance(file, str):
+            with open(file, 'r', encoding='utf-8') as f:
+                code = f.read()
+        else:
+            code = file.read()
 
-                        self.process_entries(cog_info['entries'], base_ctx, class_dict, func_dict)
+        namespace = {}
+        exec(code, namespace)
+        class_dict = {name: obj for name, obj in namespace.items() if isinstance(obj, type)}
+        func_dict = {name: obj for name, obj in namespace.items() if callable(obj)}
+
+        json_filepath = (file[:-5] + ".json") if isinstance(file, str) else (file_name[:-5] + ".json")
+        if os.path.exists(json_filepath):
+            with open(json_filepath, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                self.process_entries(data, base_ctx, class_dict, func_dict)
+        elif file_name and file_name[:-5] in class_dict:
+            class_dict[file_name[:-5]](base_ctx).init(base_ctx)
+
+    def load_cog_from_json(self, file, base_ctx):
+        data = json.load(file)
+        for cog_name, cog_info in data.items():
+            cog_file_path = cog_name if isinstance(file, str) else os.path.join(os.path.dirname(file.name), cog_name)
+            if os.path.exists(cog_file_path):
+                print(f"Load {cog_info['name']}")
+                with open(cog_file_path, 'r', encoding='utf-8') as f:
+                    namespace = {}
+                    exec(f.read(), namespace)
+                    class_dict = {name: obj for name, obj in namespace.items() if isinstance(obj, type)}
+                    func_dict = {name: obj for name, obj in namespace.items() if callable(obj)}
+
+                    self.process_entries(cog_info['entries'], base_ctx, class_dict, func_dict)
 
     def process_entries(self, entries, base_ctx, class_dict, func_dict):
         for entry in entries:
